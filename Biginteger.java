c695c360e298bd9e3096bec5eb3e02b7643d18d9
menon92 <mehadihasan80@gmail.com>
2015-09-02 16:06:01 +0600

    Update Biginteger.java

--- Biginteger.java
+++ Biginteger.java
@@ -1,12 +1,15 @@
 import java.util.*;
 import java.math.*;
 
-class Main {
+class Main 
+{
 	static Scanner input = new Scanner(System.in);
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		int N, i;
 		BigInteger AA;
-		while(input.hasNext()) { 
+		while(input.hasNext()) 
+		{ 
 			N = input.nextInt();
 			AA = input.nextBigInteger();
 			
@@ -15,7 +18,8 @@ class Main {
 			BigInteger increment = new BigInteger("1");
 			
 			int p = 1;
-			for(i = 0; i < N; i++) {
+			for(i = 0; i < N; i++)
+			{
 				sum = sum.add(ii.multiply(AA.pow(p++)));
 				ii = ii.add(increment);
 			}
